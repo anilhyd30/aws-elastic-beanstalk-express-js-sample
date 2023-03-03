6d7b0bf20c5c4cecc2497885b61f53ac39eefb54
anilhyd30 <125890670+anilhyd30@users.noreply.github.com>
2023-03-03 14:59:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Full Pipeline Testing for Anil'));
+app.get('/', (req, res) => res.send('Is the automated pipeline working??'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
