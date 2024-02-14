2154c8e3e9352e24e7b1aa20ad6e16cf2dc407e6
anilhyd30 <125890670+anilhyd30@users.noreply.github.com>
2024-02-14 22:55:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('NEW UPDATE DAT 16:32 ??'));
+app.get('/', (req, res) => res.send('OKJI CHANGES'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
